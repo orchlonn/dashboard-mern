cea54b6506843d906a78f4bf6f6fa6e7d012a826
orchlonn <orchllnz@gmail.com>
2023-02-28 23:58:16 -0800

    added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import useMemo from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout/index";
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+      shaa
+    </div>
+  );
 }
 
 export default App;
